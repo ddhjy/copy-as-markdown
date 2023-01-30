e2187d91e829b362570539d91b0dd0d18c380e57
KAI <510893492@qq.com>
2023-01-30 16:36:56 +0800

    Solve the problem that Chinese symbols are deleted

--- src/background.js
+++ src/background.js
@@ -114,17 +114,21 @@ async function handleExport(action) {
                     throw new Error(`Expecting exactly 1 tab, got ${tabs.length} items.`);
                 }
 
+                function removeNonUTF8Chars(str) {
+                    return str.replace(/[^\u4e00-\u9fa5\u3002\uff1f\uff01\uff0c\u3001\uff1b\uff1a\u201c\u201d\u2018\u2019\uff08\uff09\u300a\u300b\u3008\u3009\u3010\u3011\u300e\u300f\u300c\u300d\ufe43\ufe44\u3014\u3015\u2026\u2014\uff5e\ufe4f\uffe5\u0020-\u007E\u00A0-\u00FF]+/g, "");
+                }
+
                 const onlyOneTab = tabs[0];
                 var title = onlyOneTab.title;
                 // title = title.replace(/[\u200B-\u200D\uFEFF\u2060\u2028\u2029\u202C]+/g, '');
                 var text = title;
-                var newText = "";
-                for (var i = 0; i < text.length; i++) {
-                    var charCode = text.charCodeAt(i);
-                    if (charCode >= 0 && charCode <= 127 || (charCode >= 0x4e00 && charCode <= 0x9fff)) {
-                        newText += text[i];
-                    }
-                }
+                var newText = removeNonUTF8Chars(text);
+                // for (var i = 0; i < text.length; i++) {
+                //     var charCode = text.charCodeAt(i);
+                //     if (charCode >= 0 && charCode <= 127 || (charCode >= 0x4e00 && charCode <= 0x9fff)) {
+                //         newText += text[i];
+                //     }
+                // }
                 return markdown.linkTo(newText, onlyOneTab.url);
             }
 
